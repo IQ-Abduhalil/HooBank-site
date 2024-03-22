3e070213f176f19297879eccf2e585e2399f208a
Acer <abduhalilismoilov560@gmail.com>
2024-03-22 11:32:52 +0500

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import "./header.scss";
+
+function Header() {
+  return (
+    <header>
+      <div className="container header__container">
+        <h2>header</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
